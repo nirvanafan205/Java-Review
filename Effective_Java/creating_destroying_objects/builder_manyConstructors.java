d3ddafebb279d60b534da62a99107f979b36ff8a
nirvanafan205 <nirvanafan205@gmail.com>
2022-06-14 21:55:12 -0700

    builder patter, need to fix bug

--- Effective_Java/creating_destroying_objects/builder_manyConstructors.java
+++ Effective_Java/creating_destroying_objects/builder_manyConstructors.java
@@ -44,4 +44,150 @@ class builder_manyConstructors
 	 *}
 	 * hard to write client code when there are many paramdeters and still harder to read
 	 */
+
+	/*	Java beans Pattern (allows inconsistency, mandates mutability
+
+
+		public class NutritionsFacts
+		{
+	//parameters inizialied to default values (if any)
+	private int = -1;
+	private int = -1;
+	private int = 0;	
+	private int = 0;	
+	private int = 0;	
+	private int = 0;	
+
+	public NutritionFacts() {}
+
+	//Setters
+
+	public void setServingSize(int val)
+	{
+	servingSize = val;
+	}
+
+	public void setServings(int val)
+	{
+	servings = val;
+	}
+
+	public void setCalories(int val)
+	{
+	calories = val;
+	}
+
+	public void setFat(int val)
+	{
+	fat = val;
+	}
+
+	public void setSodium(int val)
+	{
+	sodium = val;
+	}
+
+	public void setCarbohydrate(int val)
+	{
+	carbohydrate = val;
+	}
+		}
+
+	// inside of main
+
+	NutritionFacts cocaCola = new NutritionFacts();
+
+	cocaCola.setServingSize(240);
+	cocaCola.setServings(8);
+	cocaCola.setCalories(100);
+	cocaCola.setSodium(35);
+	cocaCola.setCarbohydrate(27);
+
+	none of the disadvantages of telescoping constructor pattern
+	easier, for elligble code
+
+	DOWNSIDES
+
+	since construction is split accross multiple alls, inconsistent state partway through its construction
+	pattern precludes the possibility of making a class immutable
+
+*/
+
+
+	// Builder Pattern
+
+
+
+	public class NutritionFacts
+	{
+		private final int servingSize;
+		private final int serving;
+		private final int calories;
+		private final int fat;
+		private final int sodium;
+		private final int carbohydrate;
+
+		public static class Builder
+		{
+			private final int servingSize;
+			private final int servings;
+
+			private int calories = 0;
+			private int fat = 0;
+			private int carbohydrate = 0;
+			private int sodium = 0;
+
+			public Builder(int servingSize, int servings)
+			{
+				this.servingSize = servingSize;
+				this.servings = servings;
+			}
+
+			public Builder calories(int val)
+			{
+				calories = val; 
+				return this;
+			}
+
+			public Builder fat(int val)
+			{
+				fat = val;
+				return this;
+			}
+
+			public Builder carbohydrate(int val)
+			{
+				carbohydrate = val; 
+				return this;
+			}
+
+			public Builder sodium(int val)
+			{
+				sodium = val;
+				return this;
+			}
+
+			public NutritionFacts build()
+			{
+				return new NutritionFacts(this);
+			}
+		}
+
+		private NutritionFacts(Builder builder)
+		{
+			servingSize = builder.servingSize;
+			servings = builder.servings;
+			calories = builder.calories;
+			fat = builder.fat;
+			sodium = builder.sodium;
+			carbohydrate = builder.carbohydrate;
+		}
+	}		
+
+	public static void main(String args[])
+	{
+
+		NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+	}
+
 }
