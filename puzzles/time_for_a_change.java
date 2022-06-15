d8e1808b6fd0e21b3a3d10175d7c90688f237481
nirvanafan205 <nirvanafan205@gmail.com>
2022-06-15 11:31:44 -0700

    binary floating-point

---
+++ puzzles/time_for_a_change.java
@@ -0,0 +1,13 @@
+class time_for_a_change
+{
+	public static void main(String args[])
+	{
+		//System.out.println(2.00 - 1.10); // what does it print? 
+		//prints out 0.8999999
+		//not all decimals can be represented exactly using binary floating-point
+		//avoid float and double where exact answers are required; for simple calculations
+		//use int or long
+		
+		System.out.println((200 - 110) + " cents");
+	}
+}
