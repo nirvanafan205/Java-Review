128547d6119c3f0c261dc431b140f4673eef886d
nirvanafan205 <nirvanafan205@gmail.com>
2022-08-11 18:57:05 -0700

    design method signatures carefully

---
+++ Effective_Java/Methods/methodSignatures.java
@@ -0,0 +1,21 @@
+public class methodSignatures
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello World");
+	}
+
+	/*
+		API design hints:
+
+		Choose method names carefully
+			
+		Don't go overboard in providing convenience methods; when in doubt, leave it out
+
+		Avoid long parameter lists
+
+		for Parameter types, favor interfaces over classes
+
+		Prefer two-element enum types to boolean parameters
+	*/
+}
